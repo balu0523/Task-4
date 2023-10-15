94cffcb757e0c32ac6e34a064cf40803b75fe098
Rajula Balaji <rajulabalaji.123@gmail.com>
2023-10-15 17:17:53 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const fetchData = () => {
+    fetch("https://dummyjson.com/users")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then((actualData) => {
+        console.log("API Response:", actualData);
+        setData(actualData.users); // Assuming the array is inside a property called "users"
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        console.error("Error fetching data:", err);
+        setIsLoading(false);
+      });
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  if (isLoading) {
+    return <div className="App">Loading...</div>;
+  }
+
+  // Render the table only if data is available
+  if (!data || data.length === 0) {
+    return <div className="App">No data available.</div>;
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Dummy Data</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Sno</th>
+            <th>Profile</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Gender</th>
+            <th>Email</th>
+            <th>Username</th>
+            <th>Domain</th>
+            <th>IP</th>
+            <th>University</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((item, index) => (
+            <tr key={index}>
+              <td>{item.id}</td>
+              <td>
+                <img src={item.image} alt="no profile" height={100} />
+              </td>
+              <td>{item.firstName}</td>
+              <td>{item.lastName}</td>
+              <td>{item.gender}</td>
+              <td>{item.email}</td>
+              <td>{item.username}</td>
+              <td>{item.domain}</td>
+              <td>{item.ip}</td>
+              <td>{item.university}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
